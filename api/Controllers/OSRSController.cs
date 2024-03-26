b258574675e48c2e459546169df4abf4c9743c7f
softvasco <sprvasco@hotmail.com>
2024-03-26 17:01:19 +0000

    Add OSRS controller

---
+++ api/Controllers/OSRSController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OSRSController : ControllerBase
+    {
+        //private readonly ILogger<OSRSController> _logger;
+        //private readonly IBankAccountRepository _bankAccountRepository;
+
+        //public OSRSController(
+        //    IBankAccountRepository bankAccountRepository,
+        //    ILogger<BankAccountsController> logger)
+        //{
+        //    _bankAccountRepository = bankAccountRepository;
+        //    _logger = logger;
+        //}
+
+    }
+}
