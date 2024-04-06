4d5e7f63e180966d59989291087d7cdb6a29056c
softvasco <sprvasco@hotmail.com>
2024-04-06 22:55:46 +0100

    docker compose and docker file for webapi

--- api/Program.cs
+++ api/Program.cs
@@ -43,13 +43,13 @@ builder.Services.AddScoped<ISubCategoryRepository, SubCategoryRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
