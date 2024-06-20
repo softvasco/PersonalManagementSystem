83e2805b63b62f2722667000c9af9f477a518a87
softvasco <sprvasco@hotmail.com>
2024-06-20 17:14:05 +0100

    add osrs proxies

---
+++ Shared/Dtos/OSRS/AddProxies.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Dtos.OSRS
+{
+    public class AddProxies
+    {
+        public string Proxies { get; set; }
+    }
+}
--- Shared/Dtos/OSRS/ProxyDto.cs
+++ Shared/Dtos/OSRS/ProxyDto.cs
@@ -2,11 +2,6 @@
 {
     public class ProxyDto
     {
-        //public int Id { get; set; }
-        //public string Description { get; set; } = string.Empty;
-        //public DateTime RegistrationDate { get; set; }
-        //public decimal Kg { get; set; }
-        //public int UserId { get; set; }
-        //public bool IsEditing { get; set; }
+       
     }
 }
