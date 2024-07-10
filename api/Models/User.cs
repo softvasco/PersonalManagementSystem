0b46d368d35bcb71e57e496c05276406f33bdb53
softvasco <sprvasco@hotmail.com>
2024-07-10 17:53:14 +0100

    more refatoring

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -6,6 +6,6 @@
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
 
-        public ICollection<Category> Categories { get; set; }
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
     }
 }
